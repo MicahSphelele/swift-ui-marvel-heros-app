ba5fdfbe7ebe21c5083dfb045bbcd165ab83a7b6
Sphelele <sphemicah@gmail.com>
2021-05-08 20:44:44 +0200

    - Created a comic data model ☢️

---
+++ Marvel Heros/Models/Comic/ComicData.swift
@@ -0,0 +1,13 @@
+//
+//  ComicData.swift
+//  Marvel Heros
+//
+//  Created by Sphelele Ngubane on 2021/05/08.
+//
+
+import SwiftUI
+
+struct ComicData : Codable {
+    var count: Int
+    var results: [Comic]
+}
