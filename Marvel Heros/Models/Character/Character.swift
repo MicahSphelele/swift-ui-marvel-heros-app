c24f5959e2d6a100f2198e78f6f6502ddfd658e6
Sphelele <59883549+sphelele-ikhokha@users.noreply.github.com>
2021-05-08 20:15:44 +0200

    - Created a character model🎖

---
+++ Marvel Heros/Models/Character/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Character.swift
+//  Marvel Heros
+//
+//  Created by Sphelele Ngubane on 2021/05/08.
+//
+
+import SwiftUI
+
+struct Character {
+    var id : Int
+    var name: String
+    var description: String
+    var thumnail: [String : String]
+    var urls: [[String : String]]
+}
