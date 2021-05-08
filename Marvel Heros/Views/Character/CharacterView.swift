6c865c465f336cc17807418e59b1a0c5177234d2
Sphelele <sphemicah@gmail.com>
2021-05-08 23:02:47 +0200

    - Added Swift package (SDWebImageSwiftUI) 💊

---
+++ Marvel Heros/Views/Character/CharacterView.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterView.swift
+//  Marvel Heros
+//
+//  Created by Sphelele Ngubane on 2021/05/08.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterView()
+    }
+}
