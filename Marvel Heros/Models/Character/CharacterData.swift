1e9676002147e700424257c1b2ef74d6e5c9dfe4
Sphelele <59883549+sphelele-ikhokha@users.noreply.github.com>
2021-05-08 20:22:29 +0200

    - Created character data model ☢️n

---
+++ Marvel Heros/Models/Character/CharacterData.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterData.swift
+//  Marvel Heros
+//
+//  Created by Sphelele Ngubane on 2021/05/08.
+//
+
+import SwiftUI
+
+struct CharacterData : Codable{
+    var count: Int
+    var results: [Character]
+}
