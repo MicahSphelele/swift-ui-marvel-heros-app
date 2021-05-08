b6542b979eb95e3b7cb3ea76793975920e037130
Sphelele <sphemicah@gmail.com>
2021-05-08 19:45:55 +0200

    - Initial Commit 🦾

---
+++ Marvel Heros/Constants/Constants .swift
@@ -0,0 +1,15 @@
+//
+//  Constants .swift
+//  Marvel Heros
+//
+//  Created by Sphelele Ngubane on 2021/05/08.
+//
+
+import Foundation
+
+struct AppConstants {
+    
+    let PUBLIC_KEY = ""
+    let PRIVATE_KEY = ""
+    
+}
---
+++ Marvel Heros/ViewModels/HomeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewModel.swift
+//  Marvel Heros
+//
+//  Created by Sphelele Ngubane on 2021/05/08.
+//
+
+import SwiftUI
+import CryptoKit
+import Combine
+
+class HomeViewModel : ObservableObject {
+    
+    init() {
+        
+    }
+}
